1c5cd615419b0c467aeddfef256b370409274057
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-08-31 10:59:55 +0900

    chore: 필요없는 라우터 제거

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const testAPIRouter = require("./routes/testAPI");
 
 const app = express();
 
@@ -23,8 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
