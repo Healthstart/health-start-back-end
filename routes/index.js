ccc53b620acf08afb2ee45adebaef7bdb93ef034
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-01 13:53:12 +0900

    feat: nodemon 라이브러리 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Hello Express");
+  res.send("Hello Express and Nodemon!");
 });
 
 module.exports = router;
