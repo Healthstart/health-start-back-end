ed23624c0cc66dd0d043cb4a2c84d85489e97790
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-03 11:32:55 +0900

    feat: 회원가입 유효성 검사 추가
    - email 중복검사
    - 넘어오는 req.body 공백 검사

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,26 @@ var router = express.Router();
 const con = require("../db/mysql");
 
 router.post("/register", (req, res) => {
-  console.log("!!");
-  const sql = `INSERT INTO users(email, password, name) VALUES(?, hex(aes_encrypt(?, '${process.env.DB_ENCRYPT}')), ?)`;
+  const insertSQL = `INSERT INTO users(email, password, name) VALUES(?, hex(aes_encrypt(?, '${process.env.DB_ENCRYPT}')), ?)`;
+  const checkSQL = `SELECT * FROM users WHERE email=?`;
 
   const { email, password, name } = req.body;
 
-  con.query(sql, [email, password, name], (err, res) => {
-    if (err) throw err;
-    console.log("data insert!");
+  if (!email || !password || !name) {
+    res.send("not data!");
+    return;
+  }
+
+  con.query(checkSQL, [email], (err, data) => {
+    if (data) {
+      res.status(400).send("같은 이메일이 있습니다!");
+      return;
+    }
+    
+    con.query(insertSQL, [email, password, name], (err, data) => {
+      if (err) throw err;
+      console.log("data insert!");
+    });
   });
 });
 
