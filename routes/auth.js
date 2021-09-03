384441e407d11f03faf3895006ca8fdfa15c0c18
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-03 11:49:11 +0900

    feat: 로그인 기능 구현 및 res 메세지 json 형태로 변경

--- routes/auth.js
+++ routes/auth.js
@@ -9,19 +9,47 @@ router.post("/register", (req, res) => {
   const { email, password, name } = req.body;
 
   if (!email || !password || !name) {
-    res.send("not data!");
+    res.json({ error: "데이터가 올바르지 않습니다." });
     return;
   }
 
   con.query(checkSQL, [email], (err, data) => {
     if (data.length > 0) {
-      res.status(400).send("같은 이메일이 있습니다!");
+      res.json({ error: "동일한 이메일이 이미 존재합니다." });
       return;
     }
 
     con.query(insertSQL, [email, password, name], (err, data) => {
       if (err) throw err;
-      console.log("data insert!");
+      res.json({ success: "성공적으로 가입이 완료되었습니다." });
+    });
+  });
+});
+
+router.post("/login", (req, res) => {
+  const checkSQL = `SELECT * FROM users WHERE email=?`;
+  const compareSQL = `SELECT password FROM users WHERE password=hex(aes_decrypt(?, '${process.env.DB_ENCRYPT}'))`;
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.json({ error: "데이터가 올바르지 않습니다." });
+    return;
+  }
+
+  con.query(checkSQL, [email], (err, data) => {
+    if (data.length <= 0) {
+      res.json({ error: "존재하지 않는 이메일입니다." });
+      return;
+    }
+
+    con.query(compareSQL, [password], (err, data) => {
+      if (data.length <= 0) {
+        res.json({ error: "비밀번호가 올바르지 않습니다." });
+        return;
+      }
+
+      /* Login API */
     });
   });
 });
