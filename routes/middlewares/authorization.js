4a81b1157711c9d7115cac10ad93dd943d774f3e
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-08 16:30:32 +0900

    feat: 로그인시 토큰 발급 구현

---
+++ routes/middlewares/authorization.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    const decode = jwt.verify(token, process.env.JWT_KEY);
+
+    if (decode) {
+      res.locals.email = decode.email;
+      next();
+    } else {
+      res.status(401).json({ error: "유호하지 않습니다!" });
+    }
+  } catch (err) {
+    res.status(401).json({ error: "토큰 만료" });
+  }
+};
+
+module.exports = verifyToken;
