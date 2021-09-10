36517f53b2d42da0869f214742278482707a32b1
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-10 09:39:57 +0900

    feat: 프로필 가져오기 구현

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const con = require("../db/mysql");
+const verifyToken = require("./middlewares/authorization");
 
-router.get("/", function (req, res, next) {
-  res.send("Hello Express and Nodemon!");
+router.get("/profile", verifyToken, (req, res, next) => {
+  const sql = "SELECT name, sub_date FROM users WHERE email=?";
+  con.query(sql, [res.locals.email], (err, data) => {
+    const { name, sub_date } = data[0];
+    res.json({
+      success: "프로필을 불러옵니다",
+      data: { name, sub_date },
+    });
+  });
 });
 
 module.exports = router;
