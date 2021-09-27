6699b0e27ec3337e7c1f62769c9b1b2b894e97fc
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-27 11:24:06 +0900

    feat: 루틴 상세정보 READ 구현

--- routes/lutin.js
+++ routes/lutin.js
@@ -14,4 +14,22 @@ router.get("/preview", verifyToken, (req, res, next) => {
   });
 });
 
+router.get("/info/:id", verifyToken, (req, res, next) => {
+  const id = req.params.id;
+  const sql = "SELECT lutin_content FROM lutins WHERE lutin_id = ?";
+  con.query(sql, [id], (err, data) => {
+    if (data.length <= 0) {
+      res.status(400).json({
+        error: "데이터가 없거나 불러올 수 없습니다!",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: "루틴 상세정보를 불러옵니다.",
+      data: data[0].lutin_content,
+    });
+  });
+});
+
 module.exports = router;
