69ebc45d24cfe59bdd26c79599e0fb71e8d2b919
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-10-01 11:48:19 +0900

    chore: console.log 제거

--- routes/middlewares/authorization.js
+++ routes/middlewares/authorization.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split('Bearer ')[1];
-    console.log(token);
+    const token = req.headers.authorization.split("Bearer ")[1];
     const decode = jwt.verify(token, process.env.JWT_KEY);
 
     if (decode) {
