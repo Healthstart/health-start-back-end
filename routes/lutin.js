c05aa55ce1a4225c26f6d77f487a23ad995515a2
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-10-01 11:46:49 +0900

    feat: 루틴 즐겨찾기, 즐겨찾기 해제 구현

--- routes/lutin.js
+++ routes/lutin.js
@@ -78,4 +78,57 @@ router.get("/favorite", verifyToken, (req, res, next) => {
   });
 });
 
+router.post("/favorite/:id", verifyToken, (req, res, next) => {
+  const checkSQL =
+    "SELECT lutin_id FROM fav_lutins WHERE favoriter = ? AND lutin_id = ?";
+  const sql = "INSERT INTO fav_lutins VALUES(?, ?)";
+  const email = res.locals.email;
+  const id = req.params.id;
+
+  con.query(checkSQL, [email, id], (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: "알수 없는 에러가 발생했습니다",
+      });
+      return;
+    }
+
+    if (data.length > 0) {
+      res.status(400).json({
+        error: "이미 즐겨찾기가 되어있습니다",
+      });
+      return;
+    }
+
+    con.query(sql, [res.locals.email, req.params.id], (err, data) => {
+      if (err) {
+        res.status(400).json({
+          error: "존재하지 않는 데이터 혹은 알 수 없는 에러가 발생했습니다",
+        });
+        return;
+      }
+
+      res.status(200).json({
+        success: "즐겨찾기 성공",
+      });
+    });
+  });
+});
+
+router.delete("/favorite/:id", verifyToken, (req, res, next) => {
+  const sql = "DELETE FROM fav_lutins WHERE favoriter = ? AND lutin_id = ?";
+  con.query(sql, [res.locals.email, req.params.id], (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: "존재하지 않거나 즐겨찾기가 되어 있지 않습니다",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: "즐겨찾기가 해제되었습니다",
+    });
+  });
+});
+
 module.exports = router;
